1ad0b1fff323127184e378d794509c053ae8741f
Dmitry Rybakov <external.dmytro.rybakov@se.bosch.com>
2021-10-14 20:37:36 +0200

    Update comments

--- BPMobileMessaging/Classes/Extensions/Data+HttpBody.swift
+++ BPMobileMessaging/Classes/Extensions/Data+HttpBody.swift
@@ -5,9 +5,9 @@
 import Foundation
 
 extension Data {
+    /// Add data from a specific file to the raw http request body
     func httpBodyEncoded(boundary: String, fileName: String, contentType: HttpHeaderContentType) -> Data {
         var data = Data()
-        // Add data from a specific file to the raw http request body
         data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
         data.append("\(HttpHeaderType.contentDisposition.rawValue): form-data; name=\"file-upload-input\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
         data.append("\(HttpHeaderType.contentType.rawValue): \(contentType.rawValue)\r\n\r\n".data(using: .utf8)!)
